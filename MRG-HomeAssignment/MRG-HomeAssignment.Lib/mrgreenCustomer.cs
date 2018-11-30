2a9480dd3e8bcf8ee29c9d9898a253e2d2066a82
Henrik Häggbom <hhaggbom@gmail.com>
2018-11-30 09:09:43 +0100

    Adding mrgreenCustomer to class lib

---
+++ MRG-HomeAssignment/MRG-HomeAssignment.Lib/mrgreenCustomer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRG_HomeAssignment.Lib
+{
+    class mrgreenCustomer
+    {
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string Street { get; set; }
+
+        public int PostalCode { get; set; }
+
+        public string City { get; set; }
+
+        public int PersonalNumber { get; set; }
+    }
+}
