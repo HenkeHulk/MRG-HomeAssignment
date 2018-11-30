ce50106c83ac633406f39693c59d6163d2f4440e
Henrik Häggbom <hhaggbom@gmail.com>
2018-11-30 10:58:42 +0100

    Added Repos for redbet and mrgreen customers

---
+++ MRG-HomeAssignment/MRG-HomeAssignment.Repo/Repo/MrgreenCustomerRepo.cs
@@ -0,0 +1,77 @@
+﻿using MRG_HomeAssignment.Lib;
+using MRG_HomeAssignment.Lib.DomainClass;
+using MRG_HomeAssignment.Repo.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRG_HomeAssignment.Repo.Repo
+{
+    public class MrgreenCustomerRepo : IMrgreenCustomerRepository
+    {
+        UserContext db = new UserContext();
+
+        public IQueryable<mrgreenCustomer> All => db.MrgreenCustomers;
+
+        public void Delete(int id)
+        {
+            db.MrgreenCustomers.Remove(Find(id));
+            Save();
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+
+        public mrgreenCustomer Find(int id)
+        {
+            return db.MrgreenCustomers.Find(id);
+        }
+
+        public int Insert(mrgreenCustomer entity)
+        {
+            var existCust = Find(entity.Id);
+            if (existCust == null)
+                db.MrgreenCustomers.Add(entity);
+            else
+            {
+                existCust.Id = entity.Id;
+                existCust.Firstname = entity.Firstname;
+                existCust.Lastname = entity.Lastname;
+                existCust.Street = entity.Street;
+                existCust.PostalCode = entity.PostalCode;
+                existCust.City = entity.City;
+                existCust.PersonalNumber = entity.PersonalNumber;
+            }
+            Save();
+            return entity.Id;
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        public void Update(mrgreenCustomer entity)
+        {
+            var existCust = Find(entity.Id);
+            if (existCust == null)
+                db.MrgreenCustomers.Add(entity);
+            else
+            {
+                existCust.Id = entity.Id;
+                existCust.Firstname = entity.Firstname;
+                existCust.Lastname = entity.Lastname;
+                existCust.Street = entity.Street;
+                existCust.PostalCode = entity.PostalCode;
+                existCust.City = entity.City;
+                existCust.PersonalNumber = entity.PersonalNumber;
+            }
+            Save();
+        }
+    }
+}
---
+++ MRG-HomeAssignment/MRG-HomeAssignment.Repo/Repo/RedbetCustomerRepo.cs
@@ -0,0 +1,76 @@
+﻿using MRG_HomeAssignment.Lib;
+using MRG_HomeAssignment.Lib.DomainClass;
+using MRG_HomeAssignment.Repo.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRG_HomeAssignment.Repo.Repo
+{
+    public class RedbetCustomerRepo : IRedBetCustomerRepository
+    {
+        UserContext db = new UserContext();
+        public IQueryable<redbetCustomer> All => db.RedbetCustomers;
+
+        public void Delete(int id)
+        {
+            db.RedbetCustomers.Remove(Find(id));
+            Save();
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+
+        public redbetCustomer Find(int id)
+        {
+            return db.RedbetCustomers.Find(id);
+        }
+
+        public int Insert(redbetCustomer entity)
+        {
+            var existCust = Find(entity.Id);
+            if (existCust == null)
+                db.RedbetCustomers.Add(entity);
+            else
+            {
+                existCust.Id = entity.Id;
+                existCust.Firstname = entity.Firstname;
+                existCust.Lastname = entity.Lastname;
+                existCust.Street = entity.Street;
+                existCust.PostalCode = entity.PostalCode;
+                existCust.City = entity.City;
+                existCust.FavoriteTeam = entity.FavoriteTeam; 
+            }
+            Save();
+            return entity.Id;
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        public void Update(redbetCustomer entity)
+        {
+            var existCust = Find(entity.Id);
+            if (existCust == null)
+                db.RedbetCustomers.Add(entity);
+            else
+            {
+                existCust.Id = entity.Id;
+                existCust.Firstname = entity.Firstname;
+                existCust.Lastname = entity.Lastname;
+                existCust.Street = entity.Street;
+                existCust.PostalCode = entity.PostalCode;
+                existCust.City = entity.City;
+                existCust.FavoriteTeam = entity.FavoriteTeam;
+            }
+            Save();
+        }
+    }
+}
